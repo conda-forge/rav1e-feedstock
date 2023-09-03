abed2c840939954aa01a616d77ed53efc08aefcb
Daniel Ching <carterbox@users.noreply.github.com>
2023-09-03 12:26:17 -0500

    REF: Use cargo-c conda-package

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,5 @@
 echo ON
 
-:: cargo-c is a rust build tool that generates shared library and pkgconfig
-:: it is installed into the build prefix
-:: https://github.com/xiph/rav1e
-cargo install cargo-c
-if errorlevel 1 exit 1
-
-:: Hmm, does conda-forge have cargo-c??
-:: https://github.com/Homebrew/homebrew-core/blob/7d7fc5432ee7b16e7a7ce9f85951052f7ad55e96/Formula/rav1e.rb
 cargo cinstall --library-type cdylib --release --prefix %LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,5 @@
 set -ex
 
-# cargo-c is a rust build tool that generates shared library and pkgconfig
-# it is installed into the build prefix
-# https://github.com/xiph/rav1e
-cargo install cargo-c
-
-# Required for cross-compiling
-export PKG_CONFIG_SYSROOT_DIR=$PREFIX
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
-
-# Hmm, does conda-forge have cargo-c??
-# https://github.com/Homebrew/homebrew-core/blob/7d7fc5432ee7b16e7a7ce9f85951052f7ad55e96/Formula/rav1e.rb
 cargo cinstall --library-type cdylib --release --prefix "${PREFIX}"
 
 # rav1e includes a command line utility that cinstall will not install
