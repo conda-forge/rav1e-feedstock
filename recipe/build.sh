10ad19bcf8bb499c3b9753af0c93b71c6eaffcb5
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-03 22:09:35 -0400

    specify target

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,20 @@
 set -ex
 
-cargo cinstall --library-type cdylib --release --prefix "${PREFIX}"
+CARGO_TARGET="${HOST}"
+CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
+CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
+CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/arm64/aarch64/")"
 
-# rav1e includes a command line utility that cinstall will not install
-cargo install --path . --root "${PREFIX}"
+cargo cinstall \
+    --library-type cdylib \
+    --release \
+    --prefix "${PREFIX}" \
+    --target "${CARGO_TARGET}"
+
+# rav1e includes a command line utility that cinstall will not install          
+cargo install \
+    --path . \
+    --root "${PREFIX}" \
+    --target "${CARGO_TARGET}"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
