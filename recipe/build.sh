139b83c5d4167481b0347539e7c53faa3beff1bc
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-04 23:23:11 -0400

    Do not depend on cargo-c ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
 set -ex
 
-if [[ "${HOST}" != "ppc64le*" ]]; then
+if [[ "${HOST}" == "ppc64le*" ]]; then
+    # https://github.com/xiph/rav1e
+    # Trying to get cargo-c to get compiled for ppc64le
+    # https://github.com/conda-forge/cargo-c-feedstock/pull/1
+    # https://github.com/conda-forge/cargo-c-feedstock/pull/2
+    cargo install cargo-c
+else
     CARGO_TARGET="${HOST}"
     CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
     CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
