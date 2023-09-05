b4c21037c57fd5a63e42d295ff9cfad6f3246366
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-04 22:42:42 -0400

    Special case for ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,29 @@
 set -ex
 
-CARGO_TARGET="${HOST}"
-CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
-CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
-CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/arm64/aarch64/")"
+if [[ "${HOST}" == powerpc64le* ]]; then
+    # https://github.com/xiph/rav1e
+    # Trying to get cargo-c to get compiled for ppc64le
+    # https://github.com/conda-forge/cargo-c-feedstock/pull/1
+    # https://github.com/conda-forge/cargo-c-feedstock/pull/2
+    cargo install cargo-c
+else
+    CARGO_TARGET="${HOST}"
+    CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
+    CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
+    CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/arm64/aarch64/")"
+    CARGO_EXTRA_ARGS="--target ${CARGO_TARGET}"
+fi
 
 cargo cinstall \
     --library-type cdylib \
     --release \
     --prefix "${PREFIX}" \
-    --target "${CARGO_TARGET}"
+    ${CARGO_EXTRA_ARGS}
 
-# rav1e includes a command line utility that cinstall will not install          
+# rav1e includes a command line utility that cinstall will not install
 cargo install \
     --path . \
     --root "${PREFIX}" \
-    --target "${CARGO_TARGET}"
+    ${CARGO_EXTRA_ARGS}
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
