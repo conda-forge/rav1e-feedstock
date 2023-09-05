f513b892b2ff0e22bf089a995ae214ae2e549176
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-04 22:42:42 -0400

    Make things only for not ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,23 @@
 set -ex
 
-CARGO_TARGET="${HOST}"
-CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
-CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
-CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/arm64/aarch64/")"
+if [[ "${HOST}" != "ppc64le*" ]]; then
+    CARGO_TARGET="${HOST}"
+    CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/conda/unknown/")"
+    CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/darwin.*/darwin/")"
+    CARGO_TARGET="$(echo -n ${CARGO_TARGET} | sed "s/arm64/aarch64/")"
+    CARGO_EXTRA_ARGS="--target ${CARGO_TARGET}"
+fi
 
 cargo cinstall \
     --library-type cdylib \
     --release \
     --prefix "${PREFIX}" \
-    --target "${CARGO_TARGET}"
+    ${CARGO_EXTRA_ARGS}
 
-# rav1e includes a command line utility that cinstall will not install          
+# rav1e includes a command line utility that cinstall will not install
 cargo install \
     --path . \
     --root "${PREFIX}" \
-    --target "${CARGO_TARGET}"
+    ${CARGO_EXTRA_ARGS}
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
